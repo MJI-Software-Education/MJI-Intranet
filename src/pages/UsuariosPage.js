ce6e15506901c5ca656606f5f3a87ce448bd02f7
Ignacio Seco <ignacio.seco@inacapmail.cl>
2021-10-24 23:07:09 -0300

    Validaciones en la creación de usuarios

--- src/pages/UsuariosPage.js
+++ src/pages/UsuariosPage.js
@@ -11,6 +11,7 @@ import InputLabel from '@material-ui/core/InputLabel';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
 import Select from '@material-ui/core/Select';
+import { Run } from '../helpers/validRun';
 const useStyles = makeStyles((theme) => ({
     formControl: {
       margin: theme.spacing(1),
@@ -42,87 +43,198 @@ export const UsuariosPage = () => {
         form,
         onChange} = useUsuarios({usuarios,dispatch,colegio});
 
+        useEffect(() => {
+            blockRun();
+        }, [bool])
+
         const {nombre,apellidoP,apellidoM,run,telefono,grado,letra,rbd,usuario, email, password} = form;
     
         const classes = useStyles();
   
         const handleChange = (event) => {
             setColegio(event.target.value);
-        };
+        };  
+
+        const handleValidateNomUser = () => {
+            const letters = /^[A-Za-z]+$/;
+            if (!nombre.match(letters)) {
+                document.getElementById("nombre").innerHTML = '<div class="alert alert-warning" role="alert">'+
+                'Ingrese solo texto en el campo'+'</div>';
+            } else {
+                document.getElementById("nombre").innerHTML = '';
+            }
+        }
+
+        const handlValidateEmailUser = () =>{
+            const re = /\S+@\S+\.\S+/;
+            if ( !re.test(email) ) {
+                document.getElementById("email").innerHTML = '<div class="alert alert-warning" role="alert">'+
+                'El Email no es válido'+'</div>';
+            }else{
+                document.getElementById("email").innerHTML = '';
+            }
+        }
+
+        const handleValidateApPaternoUser = () => {
+            const letters = /^[A-Za-z]+$/;
+            if (!apellidoP.match(letters)) {
+                document.getElementById("apellidoP").innerHTML = '<div class="alert alert-warning" role="alert">'+
+                'Ingrese solo texto en el campo'+'</div>';
+            } else {
+                document.getElementById("apellidoP").innerHTML = '';
+            }
+        }
+        
+        const handleValidateApMaternoUser = () => {
+            const letters = /^[A-Za-z]+$/;
+            if (!apellidoM.match(letters)) {
+                document.getElementById("apellidoM").innerHTML = '<div class="alert alert-warning" role="alert">'+
+                'Ingrese solo texto en el campo'+'</div>';
+            } else {
+                document.getElementById("apellidoM").innerHTML = '';
+            }
+        }
+
+        const handleValidateRunUser = () => {
+            if (!Run.validaRut(run)) {
+                document.getElementById("run").innerHTML = '<div class="alert alert-warning" role="alert">'+
+                'El Run no es válido'+'</div>';
+            } else {
+                document.getElementById("run").innerHTML = '';
+            }
+        }
+
+        const handleValidateNumber = () => {
+            const onlyNumber = /^-?[\d.]+(?:e-?\d+)?$/;
+            if( !onlyNumber.test(telefono) ){
+                document.getElementById("telefono").innerHTML = '<div class="alert alert-warning" role="alert">'+
+                'Porfavor ingrese sólo números'+'</div>';
+            }else {
+                document.getElementById("telefono").innerHTML = '';
+            }
+        }
+
+        const handleSubmit = (e) => {
+
+            if ( document.getElementById("nombre").innerHTML.length > 0 ) {
+                return e.preventDefault();
+            }
+
+            if ( document.getElementById("email").innerHTML.length > 0 ) {
+                return e.preventDefault();
+            }
+
+            if ( document.getElementById("apellidoP").innerHTML.length > 0 ) {
+                return e.preventDefault();
+            }
+
+            if ( document.getElementById("apellidoM").innerHTML.length > 0 ) {
+                return e.preventDefault();
+            }
+
+            if ( document.getElementById("run").innerHTML.length > 0 ) {
+                return e.preventDefault();
+            }
+
+            if ( document.getElementById("telefono").innerHTML.length > 0 ) {
+                return e.preventDefault();
+            }
+
+            onSubmit({nombre,apellidoP,apellidoM,run,telefono,grado,letra,rbd,usuario, email, password,organizacion:colegio});
+        }
+
+        const blockRun = () => {
+
+            if (bool) {
+                document.getElementById("run1").readOnly = true;
+            } else {
+                document.getElementById("run1").readOnly = false;
+            }
+        }
+
     return (
         <div >
             <Modal
-          centered
-          visible={open}
-          closable={true}
-          onCancel={close}
-          okButtonProps={{block:true}}
-          footer={null}
-          width={550}
-        >
+                centered
+                visible={open}
+                closable={true}
+                onCancel={close}
+                okButtonProps={{block:true}}
+                footer={null}
+                width={550}
+            >
                 {
                     (bool) ?<h1>Editar</h1> :<h1>Nuevo</h1>
                 }
-           <form >
-               <div className ="formularios">
-               <div>Nombre
-               <input type="text" name="nombre" value={nombre}  onChange={onChange} autoComplete="off"    />
-               </div>
-               <div>
-                Email
-               <input type="text" name="email" value={email} onChange={onChange}  autoComplete="off"    />
-               </div>
-               <div>
-                Apellido paterno
-               <input type="text" name="apellidoP" value={apellidoP} onChange={onChange}  autoComplete="off"    />
-               </div>
-               <div>
-                Apellido materno
-               <input type="text" name="apellidoM" value={apellidoM} onChange={onChange}  autoComplete="off"    />
-               </div>
-               <div>
-                Run
-               <input type="text" name="run" value={run} onChange={onChange}  autoComplete="off"    />
-               </div>
-               <div>
-                Telefono
-               <input type="text" name="telefono" value={telefono} onChange={onChange}  autoComplete="off"    />
-               </div>
-              
-               
-               {
-                   (!bool) 
-                   && 
-                   <>
-                    <div>
-                    Grado
-               <input type="text" name="grado" value={grado} onChange={onChange}  autoComplete="off"    />
-               </div>
-               <div>
-                Letra
-               <input type="text" name="letra" value={letra} onChange={onChange}  autoComplete="off"    />
-               </div>
-               <div>
-                Usuario
-               <input type="text" name="usuario" value={usuario} onChange={onChange}  autoComplete="off"    />
-               </div>
-               <div>
-                RBD
-               <input type="text" name="rbd" value={rbd} onChange={onChange}  autoComplete="off"    />
-               </div>
-                   <div>
-                       Password
-                       <input type="text" name="password" value={password} onChange={onChange}  autoComplete="off"    />
-                   </div></>
-               }
-               
-               </div>
-               <div className="end">
-               <Button onClick={()=>onSubmit({nombre,apellidoP,apellidoM,run,telefono,grado,letra,rbd,usuario, email, password,organizacion:colegio})} type='primary' >Guardar</Button>
-               </div>
-           </form>
+                <form >
+                    <div className ="formularios">
+                        <div>
+                            Nombre
+                            <input type="text" onBlur={handleValidateNomUser} name="nombre" value={nombre} onChange={onChange} autoComplete="off"/>
+                            <span id="nombre"> </span>
+                        </div>
+                        <div>
+                            Email
+                            <input type="text" onBlur={handlValidateEmailUser} name="email" value={email} onChange={onChange}  autoComplete="off"/>
+                            <span id="email"> </span>
+                        </div>
+                        <div>
+                            Apellido paterno
+                            <input type="text" onBlur={handleValidateApPaternoUser} name="apellidoP" value={apellidoP} onChange={onChange} autoComplete="off"/>
+                            <span id="apellidoP"></span>
+                        </div>
+                        <div>
+                            Apellido materno
+                            <input type="text" onBlur={handleValidateApMaternoUser} name="apellidoM" value={apellidoM} onChange={onChange}  autoComplete="off"/>
+                            <span id="apellidoM"></span>
+                        </div>
+                        <div>
+                            Run
+                            <input type="text" id="run1" onBlur={handleValidateRunUser} name="run" value={run} onChange={onChange} autoComplete="off" placeholder="XXXXXXXX-X"/>
+                            <span id="run"></span>
+                        </div>
+                        <div>
+                            Telefono
+                            <input type="text" onBlur={handleValidateNumber} name="telefono" value={telefono} onChange={onChange}  autoComplete="off"/>
+                            <span id="telefono"></span>
+                        </div>
+                        
+                        
+                        {
+                            (!bool) 
+                            && 
+                            <>
+                                <div>
+                                    Grado
+                                    <input type="text" name="grado" value={grado} onChange={onChange} autoComplete="off" maxLength="2"/>
+                                </div>
+                                <div>
+                                    Letra
+                                    <input type="text" name="letra" value={letra} onChange={onChange} autoComplete="off" maxLength="1"/>
+                                </div>
+                                <div>
+                                    Usuario
+                                    <input type="text" name="usuario" value={usuario} onChange={onChange} autoComplete="off"/>
+                                </div>
+                                <div>
+                                    RBD
+                                    <input type="text" name="rbd" value={rbd} onChange={onChange} autoComplete="off"/>
+                                </div>
+                                <div>
+                                    Password
+                                    <input type="text" name="password" value={password} onChange={onChange} autoComplete="off"/>
+                                </div>
+                            </>
+                        }
                     
-                </Modal>
+                    </div>
+
+                    <div className="end">
+                        <Button onClick={handleSubmit} type='primary' >Guardar</Button>
+                    </div>
+                </form>
+                    
+            </Modal>
             <div className="flexEvenly">
             <h1>Usuarios</h1>
             {/* <div className="addDocentes">
